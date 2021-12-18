27339e6173beeb68a65d30e0e04383bd9a330365
Tim Pennington <ptimmy369@gmail.com>
2021-12-18 00:16:54 -0800

    troubleshooting

--- index.js
+++ index.js
@@ -1,78 +1,8 @@
-/* collapsible */
-const drumButton = document.getElementById("collapsible") 
-document.getElementById("drum-btn").addEventListener("click", function() {
-    drumButton.classList.toggle("collapsible--expanded");
-})
+/* Collapsible code */
+const collapsibles = document.querySelectorAll(".collapsible");
 
-
-
-/* drum */
-const drumSelection = document.querySelectorAll(".drum").length;
-
-for (let i = 0; i < drumSelection; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-      let eachButton = this.innerHTML;
-
-      makeSound(eachButton);
-
-      buttonAnimation(eachButton)    
-
-    })
-}
-
-document.addEventListener("keydown", function(e) {     
-
-    makeSound(e.key);
-
-    buttonAnimation(e.key);    
-
-})
-
-function makeSound(key) {
-
-       switch (key) {
-        case "a":
-        case "A":   
-            var audio = new Audio("sounds/hi-hat.wav");
-            audio.play();
-            break;
-
-        case "s":
-        case "S":   
-            var audio = new Audio("sounds/snare.wav");    
-            audio.play();
-            break;
-
-        case "d":
-        case "D":
-            var audio = new Audio("sounds/cowbell.wav");    
-            audio.play();
-            break;
-
-        case "f":
-        case "F":
-            var audio = new Audio("sounds/bass.wav");    
-            audio.play();  
-             break;
-
-        case "g":
-        case "G" :  
-          var audio = new Audio("sounds/clap.wav");    
-            audio.play();  
-             break;
-
-        default: console.log(eachButton);      
-       
-    }
-}
-
-function buttonAnimation(currentKey) {
-    let activeButton = document.querySelector("." + currentKey);
-
-    activeButton.classList.add("pressed");
-
-    setTimeout(function() {
-        activeButton.classList.remove("pressed");
-    }, 100);
-}
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
